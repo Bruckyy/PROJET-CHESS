ae8b3fb3d4d51a4e95c1bdbb335f3df64dd30ad0
HongThomas <73742517+HongThomas@users.noreply.github.com>
2021-05-17 09:43:16 +0200

    ptit correc

---
+++ Echecs/src/Case.java
@@ -0,0 +1,53 @@
+import com.modeliosoft.modelio.javadesigner.annotations.objid;
+
+
+public class Case {
+
+    private int colonne;
+    private int ligne;
+    private Piece piece;
+
+    public Case()
+    {
+    	this.piece.super();
+    	this.colonne = 0;
+    	this.ligne = 0;
+    	
+    }
+    
+    public Case(int ligne, int colonne,Piece p) {
+    	
+    }
+
+
+    public boolean CaseVide() {
+    }
+
+
+    public int getLigne() {
+    }
+
+
+    public int getColonne() {
+    }
+
+    public void setLigne(int p1) {
+    }
+
+
+    public void setColonne(int p1) {
+    }
+
+
+    public Piece getPiece() {
+    }
+
+
+    public void setPiece(Piece p1) {
+    }
+
+
+    public Case(int ligne, int colonne) {
+    }
+
+}
