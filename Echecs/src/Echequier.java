f6f5e2af59752c11b03f292524cc7556cb79ea83
LesTravaux <73838483+LesTravaux@users.noreply.github.com>
2021-05-17 10:49:11 +0200

    nettoyage+ correct to string cavalier

--- Echecs/src/Echequier.java
+++ Echecs/src/Echequier.java
@@ -2,15 +2,12 @@ import java.util.ArrayList;
 import java.util.List;
 import com.modeliosoft.modelio.javadesigner.annotations.objid;
 
-@objid ("4450187e-5a18-4f9b-8d69-31758cd74e7b")
+
 public class Echequier {
-    @objid ("3fea1e89-9fd1-4525-bafd-ba07b53f2f5f")
     public Piece[] board;
 
-    @objid ("cf622053-9a3f-43db-bc1b-739985112ba2")
     public List<Piece> piece = new ArrayList<Piece> ();
 
-    @objid ("ebada7d5-2d92-473f-9763-233acfe37a70")
     public String toString() {
     }
 
--- Echecs/src/Roi.java
+++ Echecs/src/Roi.java
@@ -1,23 +1,18 @@
 import com.modeliosoft.modelio.javadesigner.annotations.objid;
 
-@objid ("cbffdba0-d9e7-4bae-93bb-6b1713937c26")
+
 public class Roi extends Piece {
-    @objid ("070edd86-8280-4fec-a20d-c83e924a4c2d")
     private boolean echec;
 
-    @objid ("adae49d3-b920-4ea8-b418-34e9c9126a4e")
     public Roi() {
     }
 
-    @objid ("4a3ff237-c2a5-4b9d-8a53-4c358f60cf1a")
     public String getEchec() {
     }
 
-    @objid ("961d36be-f286-41d6-8b7b-c6a13fd8c04f")
     public void setEchec() {
     }
 
-    @objid ("a74999a5-c1c6-48d2-9536-4d71d84cca40")
     public String toString() {
     }
 
