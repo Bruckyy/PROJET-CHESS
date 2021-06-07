aa301bca3e546d88015d880fad2e2d2ef7ba14b9
LesTravaux <73838483+LesTravaux@users.noreply.github.com>
2021-06-07 23:48:08 +0200

    Update Jeu.java
    ajout accesseurs et constructeurs
    ajout  du parametre nomFichier

--- Echecs/src/main/Jeu.java
+++ Echecs/src/main/Jeu.java
@@ -1,12 +1,24 @@
 package main;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Jeu {
 
     private Echiquier echiquier;
-
-    public static void save() {
-    }
-
-    public void restore() {
-    }
-
+    
+    public Jeu() {}
+    public Jeu(Echiquier ech) {this.echiquier=ech;}
+    
+    public void setEchiquier(Echiquier ech) {this.echiquier=ech;}
+    public Echiquier getEchiquier() {return this.echiquier;}
+    
+    
+    public static void save(String nomFichier) {}
+    public void restore(String nomFichier) {}
+    
 }
