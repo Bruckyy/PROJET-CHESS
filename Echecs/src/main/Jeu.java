86c81f145a86f7d837baa55c6765c7238188d663
LesTravaux <73838483+LesTravaux@users.noreply.github.com>
2021-06-08 20:46:01 +0200

    Update Jeu.java

--- Echecs/src/main/Jeu.java
+++ Echecs/src/main/Jeu.java
@@ -12,13 +12,31 @@ public class Jeu {
     private Echiquier echiquier;
     
     public Jeu() {}
-    public Jeu(Echiquier ech) {this.echiquier=ech;}
+    
+    public Jeu(Echiquier ech) {
+    	this.echiquier=ech;
+    	}
     
     public void setEchiquier(Echiquier ech) {this.echiquier=ech;}
     public Echiquier getEchiquier() {return this.echiquier;}
     
     
-    public static void save(String nomFichier) {}
+    public void save(String nomFichier) {
+        try {
+            System.out.println("Sauvegarde : "+nomFichier);
+            BufferedWriter bw = Files.newBufferedWriter(Paths.get(nomFichier));
+            for(int x=8;x>0;x--) {
+            	for(int y=1;y<8;y++) {
+            		String result= this.getEchiquier().chercherCase(x,y).toString()+" ";
+            		bw.write(result);
+            	}
+            	bw.write("\n");
+            }
+            bw.close();
+        } catch(IOException e){
+            System.err.println(e);
+        }
+    }
     public void restore(String nomFichier) {}
     
 }
